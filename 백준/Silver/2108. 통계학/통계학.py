55958f320c9257c03bcb52f2af1b515e473747e6
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-07-03 20:29:51 +0900

    [Silver III] Title: 통계학, Time: 336 ms, Memory: 150644 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -6,19 +6,14 @@ num = []
 for i in range(n):
   num.append(int(input()))
 num.sort()
-
-avg = sum(num)/n
-mid = num[n//2]
-#many_items = 
 many = Counter(num).most_common(n=2)
-rng = max(num) - min(num)
-print(round(avg))#산술평균
-print(mid)#중앙값
 if len(many) > 1:
   if many[0][1] == many[1][1]:
-    print(many[1][0])#최빈값
+    m = many[1][0]#최빈값
   else:
-    print(many[0][0])
+    m = many[0][0]
 else:
-  print(many[0][0])#최빈값
-print(rng)#범위
+  m = many[0][0]#최빈값
+ans = [round(sum(num)/n), num[n//2], m, max(num) - min(num)]
+for i in ans:
+  print(i)
