721ba737029d4083f80b073109442f913af32f32
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-07-07 20:26:53 +0900

    [Gold V] Title: 테트로미노, Time: 212 ms, Memory: 119860 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,17 +1,17 @@
 import sys; input=sys.stdin.readline
-def dfs(r,c,idx,total):
+def dfs(r,c,idx,total):#x,y좌표,선택한 블록 갯수,합계
   global ans
-  if ans >= total + max_val * (3-idx):
+  if ans >= total + max_val * (3-idx): #최대 합계면 return
     return
-  if idx == 3:
-    ans = max(ans, total)
+  if idx == 3: #모든 블럭을 선택했을 때 return
+    ans = max(ans, total) 
     return
-  else:
+  else: #모든 블럭을 아직 선택하지 않았을 때
     for i in range(4):
       nx = r + dx[i]
       ny = c + dy[i]
       if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0:
-        if idx == 1:
+        if idx == 1: #두 개의 블럭을 선택했을 때 ㅗ 모양 탐색
           visited[nx][ny] = 1 #방문 체크
           dfs(r, c, idx + 1, total + graph[nx][ny])          
           visited[nx][ny] = 0
