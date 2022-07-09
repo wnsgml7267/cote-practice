c83d6188f45ebdeba2452fa4095dd53264dd411a
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-07-09 20:50:31 +0900

    [Silver V] Title: 집합, Time: 5708 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+m = int(input())
+s = set()
+for i in range(m):
+  a = input().strip().split()
+  if len(a) == 1:
+    if a[0] == "all":
+      s = set([i for i in range(1,21)])
+    else:
+      s = set()
+  else:
+    x,y = a[0], int(a[1])
+    if x == "add":
+      s.add(y)
+    elif x == "remove":
+      s.discard(y)
+    elif x == "check":
+      if y in s:
+        print(1)
+      else:
+        print(0)
+    elif x == "toggle":
+      if y in s:
+        s.discard(y)
+      else:
+        s.add(y)
+      
