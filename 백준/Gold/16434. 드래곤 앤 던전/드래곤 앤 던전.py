55f345699b4781f5a42890fd2de3f9143b789ae4
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-07-17 20:18:58 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 308 ms, Memory: 34900 KB -BaekjoonHub

---
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+room_count, attack = map(int,input().split()) #방 개수, 공격력
+ans = []
+cur_hp=0
+h_hp=0
+
+for i in range(room_count):
+  t, a, h = map(int,input().split())
+  hp=0
+  if t == 1: #a=몬스터 공격력, h=몬스터 체력
+    if h % attack == 0:
+      hp += -(a*(h//attack-1))
+    else:
+      hp += -(a*(h//attack))
+    ans.append(hp)
+  elif t == 2: #a=용사 공격력 증가, h=용사 체력회복
+    attack += a
+    hp = h
+  cur_hp += hp
+  if cur_hp > 0:
+    cur_hp=0
+  h_hp = max(h_hp,abs(cur_hp))
+print(h_hp+1)
+      
