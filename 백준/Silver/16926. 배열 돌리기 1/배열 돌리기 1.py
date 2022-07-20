d60cb737b2ede2c8667557dea03e781935912beb
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-07-20 20:49:07 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 732 ms, Memory: 116276 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -1,41 +1,37 @@
 import sys
 input = sys.stdin.readline
-
 n,m,r = map(int,input().split())
+graph = []
+for _ in range(n):
+  graph.append(list(map(int,input().split())))
 
-data = [list(map(int,input().split())) for _ in range(n)]
+def rot(x,y):
+  global temp
+  prev_value = graph[x][y] #현재 값 저장
+  graph[x][y] = temp #회전
+  temp = prev_value #이전 값 저장
+  
+  
+for _ in range(r): #회전 수
+  for i in range(min(n, m) // 2): #돌려지는 갯수
+    x,y=i,i
+    temp = graph[x][y] #시작 값 저장
 
-for _ in range(r):
-    for i in range(min(n, m) // 2):
-    	# x, y 는 돌려지는 배열중 가장 첫번째 배열 인덱스
-        x, y = i, i
-        temp = data[x][y]
-                            # 안쪽까지 계속 고려해야하기 때문에 n-i랑 m-i까지로 범위설정
-        for j in range(i + 1, n - i):  #좌
-            x = j
-            prev_value = data[x][y]
-            data[x][y] = temp
-            temp = prev_value
+    for k in range(i+1, n-i): #왼쪽
+      x = k #행을 바꿔주기 위함
+      rot(x,y)
 
-        for j in range(i + 1, m - i):  #하
-            y = j
-            prev_value = data[x][y]
-            data[x][y] = temp
-            temp = prev_value
+    for k in range(i+1, m-i): #아래
+      y = k
+      rot(x,y)
 
-        for j in range(i + 1, n - i):  #우
-            x = n - j - 1
-            prev_value = data[x][y]
-            data[x][y] = temp
-            temp = prev_value
+    for k in range(i+1, n-i): #오른쪽
+      x = n - k - 1  #행이 감소
+      rot(x,y)
 
-        for j in range(i + 1, m - i):  #상
-            y = m - j -1
-            prev_value = data[x][y]
-            data[x][y] = temp
-            temp = prev_value
+    for k in range(i+1, m-i): #
+      y = m - k - 1  #열이 감소
+      rot(x,y)
 
 for i in range(n):
-    for j in range(m):
-        print(data[i][j], end=' ')
-    print()
+  print(*graph[i])
