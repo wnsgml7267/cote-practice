f06df928e0c140d27ab9ffb77f6bbd35f72e4e7a
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-07-22 21:04:38 +0900

    [Silver I] Title: 줄어들지 않아, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.py
@@ -0,0 +1,9 @@
+t=int(input())
+dp = [[1,0,0,0,0,0,0,0,0,0] for i in range(65)]
+dp[1] = [1,1,1,1,1,1,1,1,1,1]
+for i in range(1,65):
+  for j in range(1,10):
+    dp[i][j] = dp[i][j-1] + dp[i-1][j]
+for i in range(t):
+  n=int(input())
+  print(sum(dp[n]))
