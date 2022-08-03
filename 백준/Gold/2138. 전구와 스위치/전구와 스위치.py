fb8c3a376c479daa16c74469a39b1808850f3ee8
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-08-03 22:43:22 +0900

    [Gold V] Title: 전구와 스위치, Time: 144 ms, Memory: 123380 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -1,37 +1,21 @@
 n = int(input())
-c = list(map(int,input().rstrip("\n")))
-want = list(map(int,input().rstrip("\n")))
+switch = list(map(int,input()))
+target = list(map(int,input()))
 
-def change(num):
-  if num == 0:
-    num = 1
-  else:
-    num = 0
-  return num
-
-def switch(c, cnt):
-  count = cnt
-  if count == 1:
-    c[0] = change(c[0])
-    c[1] = change(c[1])
-  for i in range(1,n):
-    if c[i-1] != want[i-1]:
-      count += 1
-      c[i-1] = change(c[i-1])
-      c[i] = change(c[i])
-      if i != n-1:
-        c[i+1] = change(c[i+1])
-  if c == want:
-    return count
-  else:
-    return -1
-res1 = switch(c[:],0)
-res2 = switch(c[:],1)
-if res1 >= 0 and res2 >= 0:
-  print(min(res1, res2))
-elif res1 >=0 and res2 < 0:
-  print(res1)
-elif res1 <0 and res2 >= 0:
-  print(res2)
-else:
-  print(-1)
+def change(a,b):
+  l = a[:]
+  cnt = 0
+  for i in range(1,n): #전구 1~n
+    if l[i-1] == b[i-1]: #이전 전구가 같으면 continue
+      continue
+    cnt += 1 #전구가 다르면 +1
+    for j in range(i-1, i+2):
+      if j<n:
+        l[j] = 1 - l[j] #스위치 변경
+  return cnt if l == b else 1e9
+ans = change(switch, target) #첫 전구 스위치 안눌렀을 때
+#첫 전구 스위치 눌렀을 때
+switch[0] = 1 - switch[0]
+switch[1] = 1 - switch[1]
+ans = min(ans, change(switch, target)+1)
+print(ans if ans != 1e9 else -1)
