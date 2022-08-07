4b24dfc9c32cd24f14a06e236c6298b78db6f958
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-08-07 21:55:13 +0900

    [Silver III] Title: 세 수 고르기, Time: 220 ms, Memory: 115136 KB -BaekjoonHub

---
+++ 백준/Silver/1503. 세 수 고르기/세 수 고르기.py
@@ -0,0 +1,29 @@
+N, M =map(int,input().split()) #자연수, 집합의 크기
+#집합의 크기 0인 경우
+answer=10**9
+arr = [True for _ in range(1002)]
+if M == 0:
+  print(0)
+else:
+  s = list(map(int,input().split())) #집합 S에 들어있는 수
+  for i in range(1,1001):
+    if i not in s:
+      start = i #첫 최소 x 값
+      break
+  for i in s:
+    arr[i] = False
+      
+  if start >= 10:
+    answer = abs(N-start**3)
+  else:
+    for k in range(start, 10):
+      if arr[k] == False:
+        continue
+      for i in range(start, 1001):
+        if arr[i] == False:
+          continue
+        for j in range(start,1002):
+          if arr[j] == False:
+            continue
+          answer = min(answer, abs(N-k*i*j))
+  print(answer)
