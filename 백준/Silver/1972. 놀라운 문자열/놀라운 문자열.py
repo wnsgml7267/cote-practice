6ef1e14c4f35df4f7b7bc58b6bc17f0a11bdbb26
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-08-09 23:38:10 +0900

    [Silver III] Title: 놀라운 문자열, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1972. 놀라운 문자열/놀라운 문자열.py
+++ 백준/Silver/1972. 놀라운 문자열/놀라운 문자열.py
@@ -1,24 +1,16 @@
 while True:
-  pair = []
   s = input()
   if s == '*':
     break
   ans = 0
-  if len(s) == 2:
-    if s[0] != s[1]:
-      ans = 0
-  elif len(s) == 1:
-    ans = 0
-  else:
+  if len(s) > 2:
     for i in range(len(s)-1):
+      pair=[]
       for j in range(len(s)-(i+1)):
         pair.append(s[j]+s[j+(i+1)])
       if len(pair) != len(set(pair)):
         ans = 1
         break
-      else:
-        pair=[]
-        continue
   if ans == 0:
     print(s + " is surprising.")
   else:
