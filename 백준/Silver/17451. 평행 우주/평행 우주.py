1e5f10bc9bb54e41524ea7eee1ab3cf20114da81
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-08-10 23:58:05 +0900

    [Silver III] Title: 평행 우주, Time: 316 ms, Memory: 64404 KB -BaekjoonHub

--- 백준/Silver/17451. 평행 우주/평행 우주.py
+++ 백준/Silver/17451. 평행 우주/평행 우주.py
@@ -1,13 +1,13 @@
 import sys
 input = sys.stdin.readline
-
-n = int(input())
-v = list(map(int, input().split()))
-answer = 0
-for i in range(n-1, -1, -1):
-    if answer <= v[i]:
-        answer = v[i]
-    else:
-        if answer % v[i]:
-            answer = (answer // v[i] + 1) * v[i]
-print(answer)
+n= int(input())
+speed = list(map(int,input().split()))
+ans = 0
+for i in range(n-1,-1,-1):
+  if ans <= speed[i]:
+    ans = speed[i]
+  else:
+    if ans % speed[i]:
+      ans = (ans // speed[i] + 1) * speed[i]
+print(ans)
+    
