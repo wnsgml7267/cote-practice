780bcd83335ec976f6aee82b4f717b94ea083fc0
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-08-12 20:10:46 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 144 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,11 +1,9 @@
 s=list(input())
-copy = s
 visit = 0
 temp = 0
 for i in range(len(s)):
   if s[i] == '<':
-    a = s[temp:i]
-    copy[temp:i] = a[::-1]
+    s[temp:i] = (s[temp:i])[::-1]
     visit = 1
   elif s[i] == '>':
     visit = 0
@@ -13,13 +11,11 @@ for i in range(len(s)):
     continue
   if visit == 0:#부등호 밖이면
     if s[i] == " ":
-      a = s[temp:i]
-      copy[temp:i] = a[::-1]
+      s[temp:i] = (s[temp:i])[::-1]
       temp = i+1
     if i == len(s)-1: #마지막에 도달하면
-      a = s[temp:i+1]
-      copy[temp:i+1] = a[::-1]
-for i in copy:
+      s[temp:i+1] = (s[temp:i+1])[::-1]
+for i in s:
   print(i,end="")
     
     
