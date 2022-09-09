a77a0e07bed4a2eff05606e494e7fafa3ff94622
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-09-09 15:41:44 +0900

    [Silver III] Title: 사탕 게임, Time: 492 ms, Memory: 116132 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,53 +1,32 @@
-import sys
-input=sys.stdin.readline
-
-def check(arr):
-    n=len(arr)
-    answer=1
-
-    for i in range(n):
-        cnt=1
-        for j in range(1, n):
-            if arr[i][j] == arr[i][j-1]:
-                cnt += 1
-            else:
-                cnt=1
-                
-            if cnt > answer:
-                answer = cnt
-
-        cnt=1
-        for j in range(1, n):
-            if arr[j][i] == arr[j-1][i]:
-                cnt += 1
-            else:
-                cnt=1
-
-            if cnt > answer:
-                answer = cnt
-
-    return answer
-
-n=int(input())
-arr=[list(input()) for _ in range(n)]
-answer=0
-
+n = int(input())
+candy = []
 for i in range(n):
-    for j in range(n):
-        if j+1 < n:
-            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
-            
-            temp=check(arr)
-
-            if temp > answer:
-                answer = temp
-            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
-
-        if i+1 < n:
-            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
-            temp=check(arr)
-            if temp > answer:
-                answer = temp
-            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
-            
+  candy.append(list(input()))
+answer = 1
+def search(candy):
+  global answer
+  for i in range(n):
+    cnt = 1
+    for j in range(1,n):
+      if candy[i][j] == candy[i][j-1]:
+        cnt += 1
+      else:
+        cnt = 1
+      answer = max(answer,cnt)
+    cnt = 1
+    for j in range(1,n):
+      if candy[j][i] == candy[j-1][i]:
+        cnt += 1
+      else:
+        cnt = 1
+      answer = max(answer,cnt)
+for i in range(1,n):
+  for j in range(1,n):
+    candy[i][j], candy[i][j-1] = candy[i][j-1], candy[i][j]
+    search(candy)
+    candy[i][j], candy[i][j-1] = candy[i][j-1], candy[i][j]
+    
+    candy[j][i], candy[j-1][i] = candy[j-1][i], candy[j][i]
+    search(candy)
+    candy[j][i], candy[j-1][i] = candy[j-1][i], candy[j][i]
 print(answer)
