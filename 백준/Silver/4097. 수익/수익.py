7ee9483995a418d7d174795a3a98771704a582b2
이준희 <82314940+wnsgml7267@users.noreply.github.com>
2022-09-20 20:47:27 +0900

    [Silver II] Title: 수익, Time: 1080 ms, Memory: 41372 KB -BaekjoonHub

--- 백준/Silver/4097. 수익/수익.py
+++ 백준/Silver/4097. 수익/수익.py
@@ -4,13 +4,18 @@ while True:
   n = int(input())
   if n == 0:
     break
-  dp = []
-  for i in range(n):
+  
+  elif n == 1:
     num = int(input())
-    dp.append(num)
-  for i in range(1,n):
-    dp[i] = max(dp[i], dp[i]+dp[i-1])
-  print(max(dp))
+    print(num)
+  else:
+    dp = []
+    for i in range(n):
+      num = int(input())
+      dp.append(num)
+    for i in range(1,n):
+      dp[i] = max(dp[i], dp[i]+dp[i-1])
+    print(max(dp))
   
     
     
