3a3e1f903d57d0e0c64421a8e28cebcb73357b27
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2022-11-08 21:35:35 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 88 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,7 +1,6 @@
 from collections import deque
 n, m = map(int,input().split())
 visited = [0 for _ in range(101)]
-visit = [False for _ in range(101)]
 graph = [i for i in range(101)]
 for i in range(n+m):
     x, y = map(int,input().split())
@@ -9,7 +8,6 @@ for i in range(n+m):
 dice = [6,5,4,3,2,1]
 q = deque()
 q.append(1)
-visit[1] = True
 cnt = 0
 while q:
     t = q.popleft()
@@ -18,8 +16,7 @@ while q:
         if 0 < td <= 100:
             if td != graph[td]:
                 td = graph[td]
-        if 0 < td <= 100 and visited[td] == 0 and visit[td] == False:
+        if 0 < td <= 100 and visited[td] == 0:
             q.append(td)
             visited[td] = visited[t] + 1
-            visit[td] = True
 print(visited[100])
