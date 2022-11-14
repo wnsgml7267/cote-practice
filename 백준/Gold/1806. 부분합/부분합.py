718ecb23eebfd556a2e9d619a63e68825d78d3fa
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2022-11-14 12:38:46 +0900

    Update 부분합.py

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,18 +1,21 @@
-N, S = map(int,input().split())
-nums = list(map(int,input().split()))
-i, j = 0, 0
-s = nums[0]
+n, s = map(int,input().split()) #길이, 합
+arr = list(map(int,input().split()))
+left, right = 0, 0
 ans = 100001
- 
+add = arr[0] #연속된 누적합
+
 while True:
-    if s >= S:
-        s -= nums[i]
-        ans = min(ans, j - i + 1)
-        i += 1
+    if add >= s: #누적합이 원하는 값(s) 이상이면
+        add -= arr[left]
+        ans = min(ans, right - left + 1) #최소 길이
+        left += 1
     else:
-        j += 1
-        if j == N:
+        right += 1
+        if right == n: #끝 지점까지 도달 시 break
             break
-        s += nums[j]
- 
-print(0) if ans == 100001 else print(ans)
+        add += arr[right]
+
+if ans == 100001:
+    print(0)
+else:
+    print(ans)
