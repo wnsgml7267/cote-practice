72d0db5e633970f37510781e99139b5ad1bc31f7
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2022-11-17 23:20:28 +0900

    [Gold IV] Title: 스도쿠, Time: 4768 ms, Memory: 155616 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,52 +1,42 @@
-from sys import stdin
-input = stdin.readline
-
-# 행 체크
-def rowCheck(r, num):
-    for c in range(9):
-        if board[r][c] == num:
+def colOk(num, x):
+    for i in range(9):
+        if sudoku[x][i] == num:
             return False
     return True
 
-# 열 체크
-def colCheck(c, num):
-    for r in range(9):
-        if board[r][c] == num:
+def rowOk(num, y):
+    for i in range(9):
+        if sudoku[i][y] == num:
             return False
     return True
 
-# 3 * 3 체크
-def squareCheck(r, c, num):
-    nr = (r//3) * 3
-    nc = (c//3) * 3
+def squareOk(num, x, y):
+    nx = (x//3) * 3
+    ny = (y//3) * 3
     for i in range(3):
         for j in range(3):
-            if board[nr+i][nc+j] == num:
+            if sudoku[nx+i][ny+j] == num:
                 return False
     return True
-
-def dfs(depth):
-    if depth == len(zeros): #0을 모두 숫자로 다 채웠을 경우 출력
-        for row in range(9):
-            for col in range(9):
-                print(board[row][col], end="")
+def dfs(d):
+    if d == len(zero):
+        for i in range(9):
+            for j in range(9):
+                print(sudoku[i][j], end='')
             print()
         exit()
-    
-    nr, nc = zeros[depth]	# 현재 확인할 위치
-    for num in range(1, 10):
-        if rowCheck(nr, num) and colCheck(nc, num) and squareCheck(nr, nc, num):
-            board[nr][nc] = num
-            dfs(depth + 1)
-            board[nr][nc] = 0
-
-
-board = []# 스도쿠 보드
-zeros = []# 0 위치 담기
-#9x9
-for r in range(9):
-    board.append(list(map(int, input().rstrip())))
-    for c in range(9):
-        if board[r][c] == 0:
-            zeros.append((r, c)) #0의 위치 기록
+    x, y = zero[d]
+    for num in range(1,10):
+        #행, 열, 3x3 중복 체크
+        if colOk(num,x) and rowOk(num,y) and squareOk(num,x,y):
+            sudoku[x][y] = num
+            dfs(d+1)
+            sudoku[x][y] = 0
+sudoku = []
+zero = []
+for i in range(9):
+    sudoku.append(list(map(int, input().rstrip())))
+    for j in range(9):
+        if sudoku[i][j] == 0:
+            zero.append((i,j))
 dfs(0)
