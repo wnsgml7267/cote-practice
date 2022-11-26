ebeac79f436a5dcabaa61387638ad984028898a1
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2022-11-26 21:51:46 +0900

    [Gold III] Title: 줄 세우기, Time: 244 ms, Memory: 37724 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 
 N, M = map(int,input().split()) # 학생들 수, 비교 횟수
 students = [[] for _ in range(N+1)] # 학생 번호
-inde = [0 for _ in range(N+1)] # 인접 차수
+inde = [0] * (N+1) # 진입 차수
 answer = []
 q = deque()
 
 for i in range(M):
     x, y = map(int,input().split())
     students[x].append(y)
-    inde[y] += 1 # 인접 차수 증가
+    inde[y] += 1 # 진입 차수 증가
 
-# 인접 차수 0 인 것들 큐에 삽입
+# 진입 차수 0 인 것들 큐에 삽입
 for i in range(1, N+1):
     if inde[i] == 0:
         q.append(i)
