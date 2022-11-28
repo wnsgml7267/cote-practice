152ef92e9cbe6930e8c342e1632d7b63eb9f4b9e
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2022-11-29 00:58:46 +0900

    [Bronze II] Title: 주사위, Time: 92 ms, Memory: 32420 KB -BaekjoonHub

--- 백준/Bronze/1233. 주사위/주사위.py
+++ 백준/Bronze/1233. 주사위/주사위.py
@@ -1,14 +1,17 @@
 from collections import Counter
-n, m, l = map(int,input().split())
-c = Counter()
-mx = 0
-ans = 0
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        for k in range(1, l+1):
-            c[i+j+k] += 1
-for i in c.keys():
-    if c[i] > mx:
-        mx = c[i]
-        ans = i
-print(ans)
+
+s = list(map(int, input().split()))
+a = []
+for i in range(1, s[0]+1):
+  for j in range(1, s[1]+1):
+    for k in range(1, s[2]+1):
+      a.append(i+j+k)
+
+num = Counter(a)
+
+max_val = max(list(num.values()))
+
+for i in list(num.keys()):
+  if num[i] == max_val:
+    print(i)
+    break
