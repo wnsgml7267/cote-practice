7edfc6c0da8a5ca3de07107f90f4d60d76bd66bf
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2022-12-30 15:44:30 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,7 @@
+ans,x,y = -1,0,0
+for i in range(9):
+    arr = list(map(int,input().split()))
+    for j in range(9):
+        if arr[j] > ans:
+            ans,x,y = arr[j],i,j
+print(ans,x+1,y+1,sep="\n")
