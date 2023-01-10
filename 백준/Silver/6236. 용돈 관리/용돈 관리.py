37fad05818201195067241d4a221a9cb1f469283
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-01-10 22:43:28 +0900

    [Silver II] Title: 용돈 관리, Time: 4244 ms, Memory: 34328 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -8,11 +8,11 @@ while lt <= rt:
     mid = (lt + rt) // 2		# 중간값. 인출할 금액
     charge = mid			# 현재 가진 돈. 처음 인출.
     num = 1				# 인출 횟수
-    for i in range(n):			# n일 살기 시작
-        if charge < daily[i]:		# 가진 돈이 부족하면 돈 인출
+    for i in daily:	# n일 살기 시작
+        if charge < i:		# 가진 돈이 부족하면 돈 인출
             charge = mid
             num += 1
-        charge -= daily[i]		# 그날 다 살음
+        charge -= i		# 그날 다 살음
 
 # m번보다 더 많이 인출하거나 인출한 금액이 하루를 다 살기에 적은 경우 (인출 금액이 적음)
     if num > m or mid < max(daily):	
