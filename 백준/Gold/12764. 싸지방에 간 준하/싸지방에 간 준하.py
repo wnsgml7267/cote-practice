0952bbd099937f0edf94755d234b21fc566e5da7
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-02-01 21:08:16 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 3500 ms, Memory: 136444 KB -BaekjoonHub

--- 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
@@ -10,8 +10,8 @@ cnt = [0 for _ in range(n)]
 cp_cnt = 0
 
 for _ in range(n):
-    p, q = map(int,input().split())
-    heapq.heappush(heap, [p,q])
+    p = list(map(int,input().split()))
+    heapq.heappush(heap, p)
 
 while heap:
     x = heapq.heappop(heap)
@@ -19,12 +19,8 @@ while heap:
         if cp[i] <= x[0]:
             if cp[i] == 0:
                 cp_cnt += 1
-                cp[i] = x[1]
-                cnt[i] += 1
-                break
-            else:
-                cp[i] = x[1]
-                cnt[i] += 1
-                break
+            cp[i] = x[1]
+            cnt[i] += 1
+            break
 print(cp_cnt)
 print(' '.join(list(map(str, cnt[0:cp_cnt]))))
