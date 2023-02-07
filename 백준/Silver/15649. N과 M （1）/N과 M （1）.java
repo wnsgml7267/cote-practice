5460c9c52b5b8d108923ac22f4350f3c01c2344d
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-02-07 14:01:40 +0900

    [Silver III] Title: N과 M (1), Time: 336 ms, Memory: 34988 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,5 +1,5 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 	
@@ -8,23 +8,29 @@ public class Main {
 	private static int R;
 	private static int[] numbers;
 	private static boolean[] isSelected;
+	private static StringBuilder sb = new StringBuilder();
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		R = sc.nextInt();
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] split = br.readLine().split(" ");
+		N = Integer.parseInt(split[0]);
+		R = Integer.parseInt(split[1]);
 		numbers = new int[R];
 		isSelected = new boolean[N+1];
 		
 		permutation(0);
+		
+		System.out.println(sb);
 	}
 	
 	private static void permutation(int cnt) {
 		if (cnt == R) {
 			for(int num : numbers) {
-				System.out.print(num + " ");
+				sb .append(num + " ");
+//				System.out.print(num + " ");
 			}
-			System.out.println();
+			sb.append("\n");
+//			System.out.println();
 			return;
 		}
 		
