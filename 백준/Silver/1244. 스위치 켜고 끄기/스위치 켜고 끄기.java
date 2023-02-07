9f018e7e08e62941d36655b066471e7edc974c96
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-02-07 23:58:26 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 132 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,61 +1,95 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    private static int N;
-    private static int[] arr;
-    private static int student_count;
-    private static int gender;
-    private static int num;
-    private static int g;
-	public static void main(String[] args) throws Exception{
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    N = Integer.parseInt(br.readLine());
-	    arr = new int[N];
-	    String[] split = br.readLine().split(" ");
-	    for(int i = 0; i < N; i++) {
-	        arr[i] = Integer.parseInt(split[i]);
-	    }
-	    
-	    student_count = Integer.parseInt(br.readLine());
-	    
-	    for(int i = 0; i < student_count; i++) {
-	    	String[] split2 = br.readLine().split(" ");
-	        gender = Integer.parseInt(split2[0]);        
-	        num = Integer.parseInt(split2[1]);
-	        if (gender == 1) {
-	        	g = num;
-	            while(g <= N) {
-	                arr[g-1] = Math.abs(arr[g-1] - 1);
-	                g += num ;
-	                
-	                
-	            }
-	        } else if (gender == 2) {
-	            arr[num-1] = Math.abs(arr[num-1] - 1);
-	            search(num-1, 1, arr);
-	        }
-	    }
-	    int cnt = 0;
-	    for(int a : arr) {
+	//결과를 한 번에 출력하기 위한 StringBuilder
+	private static StringBuilder sb = new StringBuilder();
+	private static int N_switch;
+	private static String[] switch_state;
+	private static int N_student;
+	private static int gender;
+	private static int number;
+	public static void main(String[] args) throws Exception {
+
+		/*
+		 * 1. 입력파일 읽어들이기 
+		 */
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		/*
+		 * 2. 입력파일 객체화
+		 */
+		N_switch = Integer.parseInt(in.readLine());
+		switch_state = in.readLine().split(" ");
+		N_student = Integer.parseInt(in.readLine());
+		/*
+		 * 3. 알고리즘 풀기
+		 */
+		for (int i = 0; i < N_student; i++) {
+			String[] _string = in.readLine().split(" ");
+			gender = Integer.parseInt(_string[0]);
+			number = Integer.parseInt(_string[1]);
+			switch (gender) {
+				case 1:
+					man();
+					break;
+				case 2:
+					woman();
+					break;
+			}
+		}
+		/*
+		 * 4. 정답 출력 
+		 */
+		int cnt = 0;
+	    for(String a : switch_state) {
 	    	if (cnt != 0 && cnt % 20 == 0) {
 	    		System.out.println();
 	    	}
 	        System.out.print(a + " ");
 	        cnt += 1;
 	    }
+//		sb.append(Arrays.deepToString(switch_state));
+//		System.out.println(sb);
 	}
-	private static void search(int k, int l, int[] arr) {
-	    if (k-l >= 0 && k+l <= N-1) {
-	        if(arr[k-l] == arr[k+l]) {
-	            arr[k-l] = Math.abs(arr[k-l] - 1);
-	            arr[k+l] = Math.abs(arr[k+l] - 1);
-	            search(k, l+1, arr);
-	        } else {
-	        	return;
-	        }
-	    }
-
+	
+	private static void man() {
+		int i = 1;
+		while (number * i <= N_switch) {
+			change(number * i - 1);
+			i++;
+		}
+		
 	}
+	
+	private static void woman() {
+		int i = 0;
+		change(number-1);
+		while(true) {
+			i++;
+			if ((number-1-i) >= 0 && (number-1+i) < N_switch) {
+				if (switch_state[number-1-i].equals(switch_state[number-1+i])) {
+					change(number-1-i);
+					change(number-1+i);
+				} else {
+                    break;
+                }	
+			}
+			else {
+				break;
+			}
+		}
+		
+	}
+	private static void change(int i) {
+		if (switch_state[i].equals("1")) {
+			switch_state[i] = "0";
+		}
+		else {
+			switch_state[i] = "1";
+		}
+	}	
 }
