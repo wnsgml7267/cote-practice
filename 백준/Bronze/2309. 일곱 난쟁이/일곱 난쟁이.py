196867aebfd588ad87ecba94f8206b02f3859b73
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-02-09 12:07:17 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,12 +1,8 @@
 from itertools import combinations as cb
-
 n = []
 for _ in range(9):
   n.append(int(input()))
-a=0
-
 for i in list(cb(n,7)):
-
   if sum(i) == 100:
     for j in sorted(list(i)):
       print(j)
