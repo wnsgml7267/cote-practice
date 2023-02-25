883c759ad08b21f6051b47eab5539bd2fcd4db7a
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-02-25 20:03:03 +0900

    [Gold V] Title: ABCDE, Time: 2408 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+n, m = map(int,input().split()) # 0 ~ n-1 사람 번호, 친구 관계 수
+answer = 0
+graph = [[] for _ in range(n)]
+for i in range(m):
+    x, y = map(int,input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+def dfs(node, depth):
+    global answer
+    if depth == 4:
+        answer = 1
+        return
+    
+    for i in range(len(graph[node])):
+        if not visited[graph[node][i]]:
+            visited[graph[node][i]] = True
+            dfs(graph[node][i], depth + 1)           
+            visited[graph[node][i]] = False
+
+
+for i in range(n-1):
+    visited = [False] * n
+    visited[i] = True
+
+    dfs(i, 0) # 시작지점, 깊이(모두 친구인지)
+    if answer == 1:
+        break
+print(answer)
