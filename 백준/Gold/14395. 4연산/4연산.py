0cb143ed5bff029c7cf85e83cde0089d7ba2912b
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-03-23 00:02:35 +0900

    [Gold V] Title: 4연산, Time: 76 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Gold/14395. 4연산/4연산.py
@@ -0,0 +1,39 @@
+from collections import deque, defaultdict
+
+s, t = map(int,input().split())
+visited = defaultdict(str)
+q = deque()
+q.append(s)
+if s == t:
+  print(0)
+else:
+  answer = -1
+  while q:
+    x = q.popleft()
+
+    if x == t:
+      answer = visited[x]
+      break
+    # 곱하기
+    nx = x**2
+    if 0 <= nx <= t**2 and visited[nx] == "" and s != nx:
+      visited[nx] = visited[x] + "*"
+      q.append(nx)
+
+    nx = x*2
+    if 0 <= nx <= t**2 and visited[nx] == "" and s != nx:
+      visited[nx] = visited[x] + "+"
+      q.append(nx)
+    
+    nx = 0
+    if visited[nx] == "" and s != nx:
+      visited[nx] = visited[x] + "-"
+      q.append(nx)
+
+    if x != 0:
+        nx = 1
+        if visited[nx] == "" and s != nx:
+            visited[nx] = visited[x] + "/"
+            q.append(nx)
+
+  print(answer)
