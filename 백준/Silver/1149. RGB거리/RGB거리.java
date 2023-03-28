61edc949fc039df20d431be507c798235b75d74f
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-03-28 17:57:04 +0900

    [Silver I] Title: RGB거리, Time: 288 ms, Memory: 20132 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,30 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[][] dp = new int[n][3];
-		for(int i = 0; i < n; i++) {
-			String[] split = br.readLine().split(" ");
-			for(int j = 0; j < 3; j++) {
-				dp[i][j] = Integer.parseInt(split[j]);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][3];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 3; j++) {
+				arr[i][j] = sc.nextInt();
 			}
 		}
+		int[][] dp = new int[n][3];
+		dp[0][0] = arr[0][0];
+		dp[0][1] = arr[0][1];
+		dp[0][2] = arr[0][2];
 		
 		for(int i = 1; i < n; i++) {
 			for(int j = 0; j < 3; j++) {
-				if (j == 0) dp[i][j] = Math.min(dp[i][j] + dp[i-1][j+1], dp[i][j] + dp[i-1][j+2]);
-				else if (j == 1) dp[i][j] = Math.min(dp[i][j] + dp[i-1][j-1], dp[i][j] + dp[i-1][j+1]);
-				else if (j == 2) dp[i][j] = Math.min(dp[i][j] + dp[i-1][j-1], dp[i][j] + dp[i-1][j-2]);
+				if (j == 0) {
+					dp[i][j] = Math.min(dp[i-1][j+1], dp[i-1][j+2]) + arr[i][j];
+				} else if (j == 1) {
+					dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j+1]) + arr[i][j];
+				} else if (j == 2) {
+					dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j-2]) + arr[i][j];
+				}
+				
 			}
 		}
-		int answer = 10000000;
-		for(int i = 0; i < 3; i++) {
-			answer = Math.min(answer, dp[n-1][i]);
+		long min_value = 100000000;
+		for (int i = 0; i < 3; i++) {
+			min_value = Math.min(min_value, dp[n-1][i]);
 		}
-		System.out.println(answer);
+		System.out.println(min_value);
 	}
-}  
-
+}
