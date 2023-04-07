f89469464324a309a7ca9332f537173b0318a388
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-04-07 10:17:37 +0900

    [Gold III] Title: 줄 세우기, Time: 952 ms, Memory: 162048 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.py
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.py
@@ -1,13 +1,10 @@
 from collections import defaultdict as dd
-import sys
-input = sys.stdin.readline
 n = int(input())
 arr = list(map(int,input().split()))
-
 dic = dd(int)
-dic[arr[0]] = 1
-for i in range(1, n):
-  dic[arr[i]] = 1
-  if dic[arr[i]-1]: # 연속된 번호라면
-    dic[arr[i]] = dic[arr[i]-1] + 1
+for i in range(n):
+  t = arr[i] 
+  dic[t] = 1
+  if dic[t-1]: # 연속된 번호라면
+    dic[t] = dic[t-1] + 1
 print(n - max(dic.values()))
