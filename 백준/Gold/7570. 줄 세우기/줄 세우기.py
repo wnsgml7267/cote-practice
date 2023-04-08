de7b831e0bf3dd76fd5f1e521ea86913cbfdf28b
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-04-09 03:54:36 +0900

    [Gold III] Title: 줄 세우기, Time: 836 ms, Memory: 157480 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.py
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.py
@@ -4,7 +4,5 @@ arr = list(map(int,input().split()))
 dic = dd(int)
 for i in range(n):
   t = arr[i] 
-  dic[t] = 1
-  if dic[t-1]: # 연속된 번호라면
-    dic[t] = dic[t-1] + 1
+  dic[t] = dic[t-1] + 1
 print(n - max(dic.values()))
