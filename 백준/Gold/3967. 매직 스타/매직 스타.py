ce0cf5b518a37154bb11534ac0aca858e900a8cd
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-04-18 01:01:16 +0900

    [Gold V] Title: 매직 스타, Time: 984 ms, Memory: 119696 KB -BaekjoonHub

--- 백준/Gold/3967. 매직 스타/매직 스타.py
+++ 백준/Gold/3967. 매직 스타/매직 스타.py
@@ -1,20 +1,16 @@
 import sys
 input = sys.stdin.readline
-num = [1,2,3,4,5,6,7,8,9,10,11,12]
+num = [1,2,3,4,5,6,7,8,9,10,11,12] # 1~12 숫자
 graph = [list(input().rstrip()) for _ in range(5)]
-arr = []
-cnt = 0
-visited = [False] * 13
-xy = []
-# result = copy.deepcopy(graph)
+arr = [] # 모든 줄을 순서대로 붙여서 하나의 문자열을 숫자로 표현
+cnt = 0 # 매직스타에 수가 채워져 있는 수
+visited = [False] * 13 # 1~12 중 사용한 숫자 체크
+
 for i in range(5):
     for j in range(9):
         if graph[i][j] == '.': continue
-        elif graph[i][j] == 'x':
-            arr.append(0)
-            xy.append((i,j))
+        elif graph[i][j] == 'x': arr.append(0)
         else: 
-            xy.append((i,j))
             arr.append(ord(graph[i][j])-64)
             visited[(ord(graph[i][j])-64)] = True
             cnt += 1
@@ -32,9 +28,7 @@ def check():
 
 def dfs(idx, n):
     global cnt
-    # print(n, cnt)
-    if n == (12-cnt):
-        
+    if n == (12-cnt): # 수가 다 채워졌으면 매직스타 확인
         if check():
             id = 0
             while id < 12:
@@ -47,9 +41,8 @@ def dfs(idx, n):
                 print(''.join(graph[l]))
                 
             exit(0)
-            return
         return
-        
+    
     if arr[idx] == 0:
         for i in range(1,13):
             if not visited[i]:
@@ -58,10 +51,7 @@ def dfs(idx, n):
                 dfs(idx + 1, n + 1)
                 arr[idx] = 0
                 visited[i] = False
-                
-                # st.remove(i)
     else:
         dfs(idx+1, n)
     
-
-dfs(0,0)
+dfs(0,0) # 확인할 숫자의 인덱스, 숫자를 채운 개수
