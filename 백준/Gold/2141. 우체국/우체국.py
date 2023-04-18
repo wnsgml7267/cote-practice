9f3e3c8b958f1e1226ec334550639ecf32d1483c
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-04-18 20:20:20 +0900

    [Gold IV] Title: 우체국, Time: 448 ms, Memory: 120984 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.py
+++ 백준/Gold/2141. 우체국/우체국.py
@@ -1,17 +1,16 @@
-import sys
+import sys, math
 input = sys.stdin.readline
-import math
-n=int(input())
-city = []
-total = 0
-cnt = 0
+n = int(input())
+X = []
+total_person = 0 # 전체 학생 수
 for i in range(n):
-  position, people = map(int,input().split()) #위치, 거주하는 사람
-  city.append([position,people])
-  total += people #총 인원
-city.sort()
-for i in range(n):
-  cnt += city[i][1]
-  if cnt >= math.ceil(total/2): #누적된 인구수가 중간값과 같거나 넘어가는 순간
-    print(city[i][0])
-    break
+    idx, person_cnt = map(int,input().split()) # 위치, 사람 수
+    total_person += person_cnt
+    X.append((idx, person_cnt))
+total_person = math.ceil(total_person / 2)
+X.sort()
+for town in X:
+    total_person -= town[1]
+    if total_person <= 0:
+        print(town[0])
+        break
