14be8d0037f13da10a609c9f21289c8b356beedd
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-04-26 00:24:33 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,18 @@
+from collections import defaultdict as dd
+def solution(numbers):
+    answer = ''
+    dic = dd(list)
+    num_arr = []
+    for num in numbers:
+        dic[str(num)[0]].append([int((str(num)*5)[:4]), str(num)])
+            
+    for i in dic.keys():
+        num_arr.append(i)
+        dic[i].sort(reverse=True)
+
+    num_arr.sort(reverse=True)
+    for i in num_arr:
+        for j in dic[i]:
+            answer += j[1]
+    # 10 1000 31 311 34 343 34 345
+    return str(int(answer))
