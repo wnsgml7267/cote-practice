06d0f27774a84e2d0bb086d74f7d39a54f8ea03e
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-04-27 00:09:15 +0900

    [Gold V] Title: 입국심사, Time: 232 ms, Memory: 118272 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -7,22 +7,19 @@ for i in range(n):
     arr.append(int(input()))
 arr.sort()
 start = 0
-end = min(arr) * m
-# end = 10**18 # arr의 최솟값 * m : 가장 많은 시간이 걸림
+end = min(arr) * m # arr의 최솟값 * m : 가장 많은 시간이 걸림
+# end = 10**18 
 while start <= end:
     mid = (start + end) // 2 # 시간의 최솟값
 
     friend_count = 0 # 친구 수
     for i in arr:
         friend_count += mid//i
-        if m <= friend_count: # 더 많은 친구들이 입국할 수 있다면:
-            end = mid - 1
+        if m <= friend_count: # 더 많은 친구들이 입국할 수 있다면
+            end = mid - 1 # 시간 줄이기
             result = mid
             break
-    if m <= friend_count: # 더 많은 친구들이 입국할 수 있다면
-        end = mid - 1 # 시간 줄이기
-        result = mid
-    else:
+    if m > friend_count:
         start = mid + 1
 
 print(result)
