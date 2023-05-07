42dfbcd2aece6ac26506539629350b96e3f0740a
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-05-07 18:16:29 +0900

    [Bronze I] Title: A/B - 2, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+print(a // b, end = '')
+if a % b:
+    print('.', end='')
+    i = 0
+    
+    while a % b and i < 1000: 
+        a = a % b * 10
+        i += 1
+        print(a // b, end = '')
+        
